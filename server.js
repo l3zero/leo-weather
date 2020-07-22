7ca573fe69e4d792207110c0521ddfbfd581958a
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-07-22 10:55:35 -0400

    Small package.json updates

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 require('dotenv').config()
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+const corsOptions = {
+   allowedHeaders: 'Access-Control-Allow-Headers,Origin, X-Requested-With, Content-Type, Accept',
+   origin: '*',
+}
+app.use(helmet())
+app.use(helmet.referrerPolicy({policy: 'same-origin'}))
+app.use(compression())
+app.use(cors(corsOptions))
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
